86e817b9d9817702bf786ccf171cc8303c1d9691
Franisis <f.guzmana@uniandes.edu.co>
2025-04-19 21:14:23 -0500

    19/04/25 INITIAL CODE

---
+++ periodicalConnections.py
@@ -0,0 +1,33 @@
+import os
+import time
+from dotenv import load_dotenv
+from mcstatus import MinecraftServer
+import subprocess
+
+load_dotenv()
+
+SERVER_ADDRESS= os.getenv('SERVER_ADDRESS')
+SERVER_PORT= os.getenv('SERVER_PORT')
+
+TIME_WO_PLAYERS = 0
+THRESHOLD = 30
+
+while True:
+    server = MinecraftServer.lookup(f"{SERVER_ADDRESS}:{SERVER_PORT}")
+    status = server.status()
+    if status.players.online == 0:
+        TIME_WO_PLAYERS += 1
+        print(f"NO PLAYERS. TIME W/O PLAYERS: {TIME_WO_PLAYERS} minutes")
+    else:
+        TIME_WO_PLAYERS = 0
+        print(f"Connected users: {status.players.online}")
+    
+    if TIME_WO_PLAYERS >= THRESHOLD:
+        print("TURNING OFF VM")
+        subprocess.run([
+            "gcloud", "compute", "instances", "stop",
+            "VM_NAME", "--zone", "ZONE", "--project", "PROOJECT_ID"
+        ])
+        break
+    
+    time.sleep(60*10) #Time before the script checks the server again.
